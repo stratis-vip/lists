2da7a1c5917aef225a0b10b3d3dc23eb55926bee
stratis-vip <stratis.vip@gmail.com>
2026-08-24 07:47:42 +0300

    add package file for Lists

---
+++ src/package.lisp
@@ -0,0 +1,16 @@
+;;; file LISTS:src/package.lisp -- exports from package lists
+;;; 
+;;; Code: 
+;;; 
+
+(defpackage :lists
+ (:use :cl)
+ (:export
+  :memberp
+  :cartesian-power
+  :cartesian-product
+  :permutations-without-replacement
+
+   )) 
+
+;;; LISTS:src/package.lisp ends here
