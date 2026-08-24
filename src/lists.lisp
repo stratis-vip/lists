51397bcd090a2a0d70ecf0f1bb7085d2f670e311
stratis-vip <stratis.vip@gmail.com>
2026-08-24 07:57:29 +0300

    change documentation in english

--- src/lists.lisp
+++ src/lists.lisp
@@ -22,13 +22,19 @@
         (memberp item (rest lst) :test test))))
 
 (defun cartesian-power (lst n)
-  "Returns a list with all possibles lists from itesm of LST with length N.
-   Items can repeated!
+  "Return the N-th Cartesian power of LST.
 
-   EXAMPLE (cartesian-power '(1 2) 3) => ((1 1 1) (1 1 2) (1 2 1) (1 2 2)
-                                         (2 1 1) (2 1 2) (2 2 1) (2 2 2))
+The result is a list containing all possible ordered sequences of
+length N whose elements are chosen from LST. Elements may be repeated.
 
-   Raise an error if not valid types provided."
+If N is zero, return a list containing the empty list.
+
+Examples:
+  (cartesian-power '(1 2) 3)
+  => ((1 1 1) (1 1 2) (1 2 1) (1 2 2)
+      (2 1 1) (2 1 2) (2 2 1) (2 2 2))
+
+Signals an error if LST is not a list or N is not a non-negative integer."
   (check-type lst list)
   (check-type n (integer 0))
   (if (zerop n)
@@ -39,7 +45,22 @@
                 lst))))
 
 (defun cartesian-product (&rest lsts)
-  "Επιστρέφει όλους του πιθανούς συνδυασμούς των στοιχείων των LSTS "
+  "Return the Cartesian product of LSTS.
+
+Each argument in LSTS must be a list. The result is a list containing
+all possible combinations, represented as lists, with one element taken
+from each input list.
+
+If no lists are supplied, return a list containing the empty list.
+If any input list is empty, the Cartesian product is NIL.
+
+Examples:
+  (cartesian-product '(1 2) '(a b))
+  => ((1 A) (1 B) (2 A) (2 B))
+
+  (cartesian-product '(1 2) '(a b) '(x y))
+  => ((1 A X) (1 A Y) (1 B X) (1 B Y)
+      (2 A X) (2 A Y) (2 B X) (2 B Y))"
   (dolist (lst lsts)
     (check-type lst list))
   (if (null lsts)
@@ -51,9 +72,25 @@
               (car lsts))))
 
 (defun permutations-without-replacement (lst n &key (test #'eql))
-  "Επιστρέφει λίστα με όλες τις διατεταγμένες ακολουθίες (ως λίστες) μήκους N
-   από στοιχεία του LST, ΧΩΡΙΣ επανατοποθέτηση (κάθε στοιχείο χρησιμοποιείται
-   το πολύ μία φορά ανά ακολουθία). Αν N > (length LST), επιστρέφει NIL."
+  "Return all permutations of N elements chosen from LST without replacement.
+
+Each result is an ordered sequence, represented as a list of length N.
+An element of LST can occur at most once in any individual sequence.
+The TEST function determines when two elements are considered equal
+and is used when removing an element after it has been selected.
+
+If N is zero, return a list containing the empty list.
+If N is greater than the length of LST, return NIL.
+
+Examples:
+  (permutations-without-replacement '(1 2 3) 2)
+  => ((1 2) (1 3) (2 1) (2 3) (3 1) (3 2))
+
+  (permutations-without-replacement '(1 2 3) 0)
+  => (NIL)
+
+  (permutations-without-replacement '(1 2 3) 4)
+  => NIL."
   (check-type lst list)
   (check-type n (integer 0))
   (if (zerop n)
